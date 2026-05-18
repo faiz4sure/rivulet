3005e0b4573b2074f1b5d27d564972bbacd54d0d
faiz4sure <k4621694@gmail.com>
2026-05-18 21:49:14 +0530

    feat: add plugin configuration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,6 +11,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_js::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
